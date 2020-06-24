75d301984e7a5cac98cbe032ef81884e7ef12990
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-24 05:32:18 +0000

    Deploy Yog9/SnapShot to github.com/Yog9/SnapShot.git:gh-pages

---
+++ precache-manifest.b9083c10a64926027155097303ad498c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b588d5958f582ec906d5c54a0762f87d",
+    "url": "/SnapShot/index.html"
+  },
+  {
+    "revision": "6eeb47d4257e5bc961a3",
+    "url": "/SnapShot/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "d181d5d7af02efcd3dab",
+    "url": "/SnapShot/static/js/2.c619e43e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SnapShot/static/js/2.c619e43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6eeb47d4257e5bc961a3",
+    "url": "/SnapShot/static/js/main.05b68eae.chunk.js"
+  },
+  {
+    "revision": "17f44848bce1e5215d0e",
+    "url": "/SnapShot/static/js/runtime-main.136163d9.js"
+  }
+]);
