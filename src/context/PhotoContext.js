559f0472a851e5c48a53cff9eb6481f4e7deff7a
Alfredo <alfredo.narvaez@travelport.com>
2020-08-25 12:17:19 +0200

    feat: implement localstorage solution for duplicated calls

--- src/context/PhotoContext.js
+++ src/context/PhotoContext.js
@@ -3,24 +3,46 @@ import axios from "axios";
 import { apiKey } from "../api/config";
 export const PhotoContext = createContext();
 
-const PhotoContextProvider = props => {
+const flickrService = axios.create({
+  baseURL: "https://api.flickr.com",
+  timeout: 1000,
+});
+
+const PhotoContextProvider = (props) => {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
-  const runSearch = query => {
-    axios
-      .get(
-        `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
-      )
-      .then(response => {
-        setImages(response.data.photos.photo);
-        setLoading(false);
-      })
-      .catch(error => {
-        console.log(
-          "Encountered an error with fetching and parsing data",
-          error
-        );
-      });
+  const runSearch = (query) => {
+    const photosSearchUrl = `/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`;
+    /* 
+      Another solution could be implement https://www.npmjs.com/package/axios-cache-adapter
+      but this seems rather overkill for something that can be done with local storage and/or
+      session storage if we want to preserve that longer.
+    */
+    const storedQuery = localStorage.getItem(query);
+
+    if (storedQuery) {
+      setImages(JSON.parse(storedQuery));
+      setLoading(false);
+      console.log("Serving from storage 🚚");
+    } else {
+      flickrService
+        .get(photosSearchUrl)
+        .then((response) => {
+          setImages(response.data.photos.photo);
+          setLoading(false);
+          console.log("Serving from API 🐱‍🏍");
+          localStorage.setItem(
+            query,
+            JSON.stringify(response.data.photos.photo)
+          );
+        })
+        .catch((error) => {
+          console.log(
+            "Encountered an error with fetching and parsing data",
+            error
+          );
+        });
+    }
   };
   return (
     <PhotoContext.Provider value={{ images, loading, runSearch }}>
