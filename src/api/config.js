8abf8c4798555f315cd01cc553576c8f03a6c49f
Alfredo <alfredo.narvaez@travelport.com>
2020-08-25 15:51:48 +0200

    feat: include new apikey for mapbox

--- src/api/config.js
+++ src/api/config.js
@@ -1,1 +1,5 @@
-export const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
+export default {
+  flickrApiKey: "636e1481b4f3c446d26b8eb6ebfe7127",
+  mapboxApiKey:
+    "pk.eyJ1IjoiYWxmZG9jaW1vIiwiYSI6ImNrZTl2bHNuMTB6MmUyeXBpbGppNmNnNjUifQ.D4LjMLx8rdLP6zsGwDnWzw",
+};
