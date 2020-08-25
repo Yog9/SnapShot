f9a77a96cba2d300a2261ca02906993b2a49b379
Alfredo <alfredo.narvaez@travelport.com>
2020-08-25 15:53:05 +0200

    feat: implementation of mapbox and react-tooltip for new feature

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,30 @@
 import React from "react";
 import NoImages from "./NoImages";
 import Image from "./Image";
+import ReactTooltip from "react-tooltip";
+import MapPopoever from "./MapPopover";
+
+/*
+  Decided to use ReactTooltip as the bundle size is relatively small:
+   26.8 kB MINIFIED
+   And the API was very developer-friendly.
+   It suited my needs for the new feature.
+*/
 
 const displayImages = (images) =>
-  images.map(({ farm, server, id, secret, title }) => {
+  images.map(({ farm, server, id, secret, title, latitude, longitude }) => {
     const url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
-    return <Image url={url} key={id} alt={title} />;
+    return (
+      <>
+        <a data-tip data-for={id}>
+          <Image url={url} key={id} alt={title} />
+        </a>
+        <ReactTooltip id={id} type="light">
+          <span>{title}</span>
+          <MapPopoever coordinates={[longitude, latitude]} />
+        </ReactTooltip>
+      </>
+    );
   });
 
 const Gallery = ({ data }) => (
