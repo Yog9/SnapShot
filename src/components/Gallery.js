1df6165c5dc1289c490cb4886d84e6a76cd4c41b
Uladzimir <raer.camp@gmail.com>
2020-11-03 10:48:13 +0100

    refactor: destructuring inside Gallery.js file

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -7,12 +7,7 @@ const Gallery = props => {
   let noImages;
   // map variables to each item in fetched image array and return image component
   if (results.length > 0) {
-    images = results.map(image => {
-      let farm = image.farm;
-      let server = image.server;
-      let id = image.id;
-      let secret = image.secret;
-      let title = image.title;
+    images = results.map(({ farm, server, id, secret, title }) => {
       let url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
       return <Image url={url} key={id} alt={title} />;
     });
