961b1f3409eea744b7ea044130435586af247b68
Victoria Diaz <mavidiaz282@gmail.com>
2020-11-10 22:05:22 +0100

    3. avoid unnecessary API requests

--- src/context/PhotoContext.js
+++ src/context/PhotoContext.js
@@ -6,14 +6,21 @@ export const PhotoContext = createContext();
 const PhotoContextProvider = props => {
   const [images, setImages] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(new Map());
   const runSearch = query => {
-    axios
+    if (data.has(query)) {
+      setImages(data.get(query));
+      setLoading(false);
+    } else {
+      axios
       .get(
         `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`
       )
       .then(response => {
         setImages(response.data.photos.photo);
         setLoading(false);
+        const aux = data.set(query, response.data.photos.photo);
+        setData(aux);
       })
       .catch(error => {
         console.log(
@@ -21,6 +28,8 @@ const PhotoContextProvider = props => {
           error
         );
       });
+    }
+    
   };
   return (
     <PhotoContext.Provider value={{ images, loading, runSearch }}>
