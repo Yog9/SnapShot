c3e2dee3f75be9a360ac38946dcb820f3c0a4001
Natalie <natalie@emeraldstay.com>
2021-05-10 14:39:21 +0200

    remove text after search submitted

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   render() {
     return (
       <PhotoContextProvider>
-        <HashRouter basename="/SnapScout">
+        <HashRouter basename="/">
           <div className="container">
             <Route
               render={props => (
