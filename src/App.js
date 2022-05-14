272005b4013c07544d02f0490944929e27b52702
AnuchitO <anuchit.prasertasng@gmail.com>
2022-05-14 22:38:11 +0700

    upgrade libs to react version 18

--- src/App.js
+++ src/App.js
@@ -1,60 +1,64 @@
-import React, { Component } from "react";
-import PhotoContextProvider from "./context/PhotoContext";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
-import Header from "./components/Header";
-import Item from "./components/Item";
-import Search from "./components/Search";
-import NotFound from "./components/NotFound";
+import React, {Component} from 'react'
+import PhotoContextProvider from './context/PhotoContext'
+import {
+  BrowserRouter,
+  HashRouter,
+  Route,
+  Routes,
+  Navigate,
+} from 'react-router-dom'
+
+import Header from './components/Header'
+import Item from './components/Item'
+import Search from './components/Search'
+import NotFound from './components/NotFound'
 
 class App extends Component {
   // Prevent page reload, clear input, set URL and push history on submit
   handleSubmit = (e, history, searchInput) => {
-    e.preventDefault();
-    e.currentTarget.reset();
-    let url = `/search/${searchInput}`;
-    history.push(url);
-  };
+    e.preventDefault()
+    e.currentTarget.reset()
+    let url = `/search/${searchInput}`
+    history.push(url)
+  }
 
   render() {
     return (
       <PhotoContextProvider>
         <HashRouter basename="/SnapScout">
           <div className="container">
-            <Route
-              render={props => (
-                <Header
-                  handleSubmit={this.handleSubmit}
-                  history={props.history}
-                />
-              )}
-            />
-            <Switch>
+            <Routes>
+              {/* TODO: fix the pre-critetia */}
               <Route
-                exact
-                path="/"
-                render={() => <Redirect to="/mountain" />}
+                element={(props) => (
+                  <Header
+                    handleSubmit={this.handleSubmit}
+                    history={props.history}
+                  />
+                )}
               />
+              <Route path="/" element={<Navigate replace to="/mountain" />} />
 
               <Route
                 path="/mountain"
-                render={() => <Item searchTerm="mountain" />}
+                element={<Item searchTerm="mountain" />}
               />
-              <Route path="/beach" render={() => <Item searchTerm="beach" />} />
+              {/*  <Route path="/beach" render={() => <Item searchTerm="beach" />} />
               <Route path="/bird" render={() => <Item searchTerm="bird" />} />
               <Route path="/food" render={() => <Item searchTerm="food" />} />
               <Route
                 path="/search/:searchInput"
-                render={props => (
+                render={(props) => (
                   <Search searchTerm={props.match.params.searchInput} />
                 )}
               />
-              <Route component={NotFound} />
-            </Switch>
+              <Route component={NotFound} /> */}
+            </Routes>
           </div>
         </HashRouter>
       </PhotoContextProvider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
